a8284005368595800fc3d117900c03380e2ccf8b
ShuvoProgram <shuvoprogramer@gmail.com>
2023-12-19 14:06:43 +0600

    Added product schema with name, price, quantity, and URL fields. 🛒💡 Fixed name minimum character validation. 🛠️ Included URL field in product schema. 🌐🔍 Refactored product schema for better readability. 🔄✨ Updated product schema with name and price fields. 📝💰

---
+++ models/product.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const schemaProduct = mongoose.Schema({
+    name: {
+        type: String,
+        min: [3, 'Name must contain 3 character.'],
+        required: [true, "Name required"],
+        trim: true,
+    },
+    price: {
+        type: Number,
+    },
+    quantity: {
+        type: Number,
+    },
+    url: {
+        type: String,
+    }
+});
+
+const Product = mongoose.model('products', schemaProduct);
+
+module.exports = Product;
